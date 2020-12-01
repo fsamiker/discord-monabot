d50845bec1558ea66b19ff14e559545f23cc7e33
Bowie Gan <strangeporcupine@gmail.com>
2020-12-01 20:33:59 +0800

    Hotfixed import errors

--- bot/mona_bot.py
+++ bot/mona_bot.py
@@ -1,3 +1,7 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from bot.cogs.errors import ErrorHandler
 from bot.cogs.resin import Resin
 from bot.cogs.reminders import Reminders
