ba259f2f5261b238cd384b04b3c3e4a6f4eebbf4
Bowie Gan <strangeporcupine@gmail.com>
2020-12-18 22:21:54 +0800

    Added db options

--- data/db.py
+++ data/db.py
@@ -9,7 +9,13 @@ load_dotenv()
 
 
 DATABASE_URI = os.getenv('DATABSE_URI')
-engine = create_engine(DATABASE_URI)
+engine = create_engine(
+    DATABASE_URI,
+    pool_size=20,
+    max_overflow=2,
+    pool_recycle=300,
+    pool_pre_ping=True,
+    pool_use_lifo=True)
 Session = sessionmaker(bind=engine)
 Base = declarative_base()
 
