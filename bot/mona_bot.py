dd96f3614ded707d3fb09a56c11397c5afa8ffd1
Bowie Gan <strangeporcupine@gmail.com>
2020-12-21 17:55:43 +0800

    Restructured cog folders

--- bot/mona_bot.py
+++ bot/mona_bot.py
@@ -2,21 +2,21 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from bot.cogs.errors import ErrorHandler
+from bot.cogs.main.errors import ErrorHandler
 from bot.cogs.resin import Resin as ResinCog
 from bot.cogs.reminders import Reminders as RemindersCog
-from bot.cogs.greetings import Greetings as GreetingsCog
-from bot.cogs.talent import Talents as TalentCog
-from bot.cogs.material import Materials as MaterialCog
-from bot.cogs.flair import Flair as FlairCog
-from bot.cogs.game import Game as GameCog
-from bot.cogs.core import Core as CoreCog
-from bot.cogs.admin import Admin as AdminCog
-from bot.cogs.character import Characters as CharacterCog
-from bot.cogs.enemy import Enemies as EnemyCog
-from bot.cogs.weapon import Weapons as WeaponCog
-from bot.cogs.artifact import Artifacts as ArtifactCog
-from bot.cogs.domain import Domains as DomainCog
+from bot.cogs.main.greetings import Greetings as GreetingsCog
+from bot.cogs.database.talent import Talents as TalentCog
+from bot.cogs.database.material import Materials as MaterialCog
+from bot.cogs.main.flair import Flair as FlairCog
+from bot.cogs.minigame.game import Game as GameCog
+from bot.cogs.main.core import Core as CoreCog
+from bot.cogs.main.admin import Admin as AdminCog
+from bot.cogs.database.character import Characters as CharacterCog
+from bot.cogs.database.enemy import Enemies as EnemyCog
+from bot.cogs.database.weapon import Weapons as WeaponCog
+from bot.cogs.database.artifact import Artifacts as ArtifactCog
+from bot.cogs.database.domain import Domains as DomainCog
 
 import os
 
